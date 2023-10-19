c3b4ce3287092f65b70737d8c63d7c80dc36145a
전지환 <jeonji0109@naver.com>
2023-10-19 22:39:13 +0900

    231019 BOJ 2579 solved

--- silver/stepOver_2579/Main.java
+++ silver/stepOver_2579/Main.java
@@ -3,41 +3,51 @@ package silver.stepOver_2579;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-	static int arr[], N, ans, sum = 0;
+	static int arr[], dp[], N;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N+3];
-
-		for (int i = 3; i < N + 3; i++) {
+		
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			sum = sum + arr[i];
 		}
-		ans = sum;
-
-//		System.out.println((Arrays.toString(arr)));
-
-		dfs(N+3, 0);
-		System.out.println(sum - ans);
-	}
-
-//	가지 않은 계단의 점수를 구하기 (거꾸로 생각하기)
-//	step: 이번 단계, score: 이전 단계까지의 점수
-	private static void dfs(int step, int score) {
-		if (step==N+3 ||arr[step] == 0 && ans > score) {
-			ans = score;
-			return; 
-		}
-		if (ans < score)
+		
+		if(N==1) {
+			System.out.println(arr[0]);
 			return;
-
-		dfs(step - 3, score + arr[step - 3]);
-		dfs(step - 2, score + arr[step - 2]);
-	}
-
+			
+		}else if(N==2) {
+			System.out.println(arr[0]+arr[1]);
+			return;
+			
+		}else {
+//			2*N 길이의 배열에서, 홀수 인덱스에는 L(longTerm)데이터를,짝수 인덱스에는 S(ShortTerm)데이터를 넣자.
+			
+//			dp 초기화 작업
+			dp = new int[2*N];
+			dp[0] = dp [1] = arr[0];
+			dp[2] = arr[1];
+			dp[3] = arr[0] + arr[1];
+			
+			for (int i = 2; i<N; i++) {
+				dp[2*i+1] = arr[i] + dp[2*(i-1)];
+				if (dp[2*(i-2)] > dp[2*(i-2)+1]) {
+					dp[2*i] = arr[i] + dp[2*(i-2)];
+				}else {
+					dp[2*i] = arr[i] + dp[2*(i-2)+1];
+				}
+			}
+			
+			if(dp[2*(N-1)]>dp[2*(N-1)+1]) {
+				System.out.println(dp[2*(N-1)]);
+			}else {
+				System.out.println(dp[2*(N-1)+1]);
+			}	
+		}	
+	}	
 }
