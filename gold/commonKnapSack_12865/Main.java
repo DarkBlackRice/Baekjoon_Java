82819ea09fbbf13bd73046001d92d0f2a423cd52
전지환 <jeonji0109@naver.com>
2023-11-02 17:55:34 +0900

    231102 BOJ 12865 modified

--- gold/commonKnapSack_12865/Main.java
+++ gold/commonKnapSack_12865/Main.java
@@ -1,9 +1,46 @@
 package gold.commonKnapSack_12865;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main {
+	
+	
+	static class Item implements Comparable<Item>{
+		int weight, value;
+
+		public Item(int weight, int value) {
+			super();
+			this.weight = weight;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Item o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+		
+		
+	}
+	
+	static int N, K;
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < N; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			
+		}
 
 	}
 
