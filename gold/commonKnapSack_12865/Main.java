a2bb9217f9572cdeb27a466f2a25d1ab6d5ef29d
전지환 <jeonji0109@naver.com>
2023-11-03 00:14:55 +0900

    231102 BOJ 12865 failed and 1181 solved

--- gold/commonKnapSack_12865/Main.java
+++ gold/commonKnapSack_12865/Main.java
@@ -3,29 +3,16 @@ package gold.commonKnapSack_12865;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	
-	static class Item implements Comparable<Item>{
-		int weight, value;
 
-		public Item(int weight, int value) {
-			super();
-			this.weight = weight;
-			this.value = value;
-		}
-
-		@Override
-		public int compareTo(Item o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-		
-		
-	}
-	
-	static int N, K;
+	static int N, K, w;
+	static int dp[], comp[], weight[];
+	static boolean flag;
 
 	public static void main(String[] args) throws IOException {
 		
@@ -35,12 +22,39 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
+		weight = new int[N];
+		
 		for (int i = 0; i < N; i++) {
-			
+		
 			st = new StringTokenizer(br.readLine());
-			
+			w = Integer.parseInt(st.nextToken());
+			weight[i] = w;
 			
 		}
+		
+		Arrays.sort(weight);
+		dp = new int[K+1];
+		
+		
+		for (int i = 0; i <= K; i++) {
+			flag=false;
+			comp = new int[N];
+			Arrays.fill(comp, Integer.MAX_VALUE);
+			for (int j = 0; j < N; j++) {
+				w = weight[j];
+				if (i<w) break;
+				flag = true;
+				comp[j] = dp[i-w] + 1;
+			}
+			if(flag) {
+				dp[i] = Integer.MAX_VALUE;
+				for (int j = 0; j < N; j++) {
+					if(dp[i] > comp[j]) dp[i] = comp[j];
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(dp));
 
 	}
 
---
+++ silver/sortingWords_1181/Main.java
@@ -0,0 +1,46 @@
+package silver.sortingWords_1181;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Main {
+	
+	static int N;
+	static String tmp, strArr[];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		strArr = new String[N];
+		
+		for (int i = 0; i < N; i++) {
+			tmp = br.readLine();
+			strArr[i] = tmp;
+		}
+		
+		
+		Arrays.sort(strArr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length() == o2.length()) {
+					return o1.compareTo(o2);
+				}else {
+					return o1.length() - o2.length();
+				}
+			}
+		});
+		
+		System.out.println(strArr[0]);
+		for (int i = 1; i < strArr.length; i++) {
+			if(!strArr[i].equals(strArr[i-1])) {
+				System.out.println(strArr[i]);
+			}
+		}
+	}
+
+}
